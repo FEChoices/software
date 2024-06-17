2cfe4e6bb8db775e56c8c5a17791f8dc75f7f921
CaioBucciarelli <caio.bucciarelli@gmail.com>
2024-06-17 14:25:20 -0300

    Desenvolvimento do login e cadastro completos

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+/*************************************** MENU HAMBURGUER ***************************************/
+
 function clickMenu() {
   const aside = document.getElementById('aside');
 
@@ -5,5 +7,40 @@ function clickMenu() {
     aside.style.display = 'none';
   } else {
     aside.style.display = 'block';
+  }
+}
+
+/*************************************** VERIFICAÇÃO LOGIN / CADASTRO ***************************************/
+
+function validarCadastro(){
+  const usuarioCd = document.getElementById('usuarioCd').value;
+  const senhaCd = document.getElementById('senhaCd').value;
+  const confirmarSenha = document.getElementById('confirmarSenha').value;
+
+  if(senhaCd != confirmarSenha) {
+    alert("As senhas estão diferentes.");
+  }
+  else if(!usuarioCd || !senhaCd || !confirmarSenha) {
+    alert('Preencha todos os campos.');
+  }
+  else{
+    alert("Cadastro concluído!");
+  }
+}
+
+
+// REVISAR ESSE CÓDIGO
+const usuarioLg = document.getElementById('nome-lg').value;
+const senhaLg = document.getElementById('senha-lg').value;
+const dica = document.getElementById('dica');
+
+function validarLogin(){
+  if(usuarioLg === usuarioCd && senhaLg === senhaCd){
+    alert("Login com sucesso!");
+    dica.innerText = "" 
+  }
+  else{
+    alert("Login errado!");
+    dica.innerText = "Dica: números"
   }
 }
