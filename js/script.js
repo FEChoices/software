656be0cc4c1883319d1b209c467a299f7a1828a0
CaioBucciarelli <caio.bucciarelli@gmail.com>
2024-06-17 11:20:01 -0300

    Conteúdo, estilização e funcionamento em js Aside

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function clickMenu() {
+  const aside = document.getElementById('aside');
+
+  if (aside.style.display == 'block') {
+    aside.style.display = 'none';
+  } else {
+    aside.style.display = 'block';
+  }
+}
