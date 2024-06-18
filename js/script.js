d061db4e820718c16e0b90f20385d2a9507b0697
CaioBucciarelli <caio.bucciarelli@gmail.com>
2024-06-17 22:22:52 -0300

    adição do slide

--- js/script.js
+++ js/script.js
@@ -60,3 +60,33 @@ function ativarPopulares(event) {
 function eventoPopulares() {
   populares.addEventListener('click', ativarPopulares)
 }
+
+/*************************************** CARROSSEL ***************************************/
+
+let idx = 0;
+
+const imagens = document.querySelectorAll('.item-carrossel')
+
+const btnVoltar = document.querySelector('#prev-img')
+btnVoltar.addEventListener('click', () => {
+    idx--
+    if (idx < 0) {
+        idx = imagens.length - 1
+    }
+    carrosel(idx)
+})
+
+const btnAvancar = document.querySelector('#next-img')
+btnAvancar.addEventListener('click', () => {
+    idx++
+    if (idx > imagens.length - 1) {
+        idx = 0;
+    }
+    carrosel(idx)
+})
+
+function carrosel(idx) {
+    const carroselImgs = document.querySelector('.container-cards');
+    const tmnItem = document.querySelector('.item-carrossel').clientWidth;
+    carroselImgs.style.transform = `translateX(${-idx * tmnItem}px)`
+}
